dafad0fa37119cf0bc708c32709919ff03d4927a
thalvari <tuomas.halvari@helsinki.fi>
2019-10-23 21:03:09 +0300

    small changes

--- examples/filter_examples/run_gaussian_noise_digits_example.py
+++ examples/filter_examples/run_gaussian_noise_digits_example.py
@@ -8,9 +8,9 @@ from dpemu.utils import get_project_root
 
 
 def main():
-    digits = load_digits()
-    x = digits["data"][0]
-    y = digits["target"][0]
+    x, y = load_digits(return_X_y=True)
+    x = x[1]
+    y = y[1]
 
     min_val = np.amin(x)
     max_val = np.amax(x)
@@ -22,6 +22,9 @@ def main():
         img_node.addfilter(GaussianNoise("mean", "std"))
         img_node.addfilter(Clip("min_val", "max_val"))
         res = img_node.generate_error(x, {"mean": 0, "std": std_steps[i], "min_val": min_val, "max_val": max_val})
+        res = np.round(res)
+        print(res.dtype)
+        print(res)
         ax.imshow(res.reshape((8, 8)), cmap="gray_r")
         ax.axis("off")
         ax.set_title(f"Std: {std_steps[i]}")

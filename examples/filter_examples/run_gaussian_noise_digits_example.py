e30d4061566146879bf2cbe8f4f6bf954e454fa5
thalvari <tuomas.halvari@helsinki.fi>
2019-10-26 15:51:17 +0300

    small changes

--- examples/filter_examples/run_gaussian_noise_digits_example.py
+++ examples/filter_examples/run_gaussian_noise_digits_example.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.datasets import load_digits
+from keras.datasets import mnist
 
 from dpemu.filters.common import GaussianNoise, Clip
 from dpemu.nodes import Array
@@ -8,24 +8,27 @@ from dpemu.utils import get_project_root
 
 
 def main():
-    x, y = load_digits(return_X_y=True)
-    x = x[1]
-    y = y[1]
+    # x, y = load_digits(return_X_y=True)
+    # shape = (8, 8)
+    (x_train, y_train), _ = mnist.load_data()
+    x_train = x_train.astype(np.float64)
+    shape = (28, 28)
 
-    min_val = np.amin(x)
-    max_val = np.amax(x)
-    std_steps = np.round(np.linspace(0, max_val, num=8), 3)
+    k = 0
+    x = x_train[k]
+    y = y_train[k]
 
-    fig, axs = plt.subplots(2, 4, constrained_layout=True)
+    max_val = np.amax(x_train)
+    std_steps = np.round(np.linspace(0, max_val, num=6), 3)
+
+    fig, axs = plt.subplots(2, 3, constrained_layout=True)
     for i, ax in enumerate(axs.reshape(-1)):
-        img_node = Array(reshape=(8, 8))
+        img_node = Array(reshape=shape)
         img_node.addfilter(GaussianNoise("mean", "std"))
         img_node.addfilter(Clip("min_val", "max_val"))
-        res = img_node.generate_error(x, {"mean": 0, "std": std_steps[i], "min_val": min_val, "max_val": max_val})
+        res = img_node.generate_error(x, {"mean": 0, "std": std_steps[i], "min_val": 0, "max_val": max_val})
         res = np.round(res)
-        print(res.dtype)
-        print(res)
-        ax.imshow(res.reshape((8, 8)), cmap="gray_r")
+        ax.imshow(res.reshape(shape), cmap="gray_r")
         ax.axis("off")
         ax.set_title(f"Std: {std_steps[i]}")
     fig.suptitle(f"Label: {y}")
